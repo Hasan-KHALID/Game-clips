7f539b122a7f408941b18ca2862324959ea37551
Hasan-KHALID <khalidhasan842@gmail.com>
2022-09-28 17:45:52 +0600

    tab component added

---
+++ src/app/shared/tabs-container/tabs-container.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit, ContentChildren } from '@angular/core';
+import { TabComponent } from '../tab/tab.component';
+
+@Component({
+  selector: 'app-tabs-container',
+  templateUrl: './tabs-container.component.html',
+  styleUrls: ['./tabs-container.component.css']
+})
+export class TabsContainerComponent implements OnInit {
+
+  @ContentChildren(TabComponent) tabs ={}
+
+  constructor() { }
+
+  ngOnInit(): void {
+    console.log(this.tabs)
+  }
+
+}
